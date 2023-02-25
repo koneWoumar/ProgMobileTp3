c0f6810b92e990b0181121decd13b67053aad9ed
koneWoumar <kone.wolouho@gmail.com>
2023-02-25 04:10:18 +0100

    coding the backend of question4 done successfully

--- app/src/main/java/com/example/myapplicationtp3/MainActivity2_question_4.java
+++ app/src/main/java/com/example/myapplicationtp3/MainActivity2_question_4.java
@@ -2,8 +2,19 @@ package com.example.myapplicationtp3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity2_question_4 extends AppCompatActivity {
 
@@ -11,15 +22,26 @@ public class MainActivity2_question_4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activity2_question4);
+        //
+        EditText champ_text = findViewById(R.id.content_q4);
+        String filename ="konewolouhooumar_angorakouame";
+        //pré-remplissage du contenu du champs text avec le contenu du fichier creer en question 1-3
+        this.createFileIfNoteExiste(filename); //creation du fichier s'il n'existe pas
+        String content_of_file = this.readContentOfFile(filename);
+        champ_text.setText(content_of_file);
+        //setting the code of the ok button
         //bouton Ok
         Button ok = (Button) findViewById(R.id.btnok);
         ok.setOnClickListener(view -> {
-            //
+            String contenu_champs = champ_text.getText().toString();
+            this.writeToFile(contenu_champs,filename);
         });
+        //setting the code of the button cancel
         //bouton cancel
         Button cancel = (Button) findViewById(R.id.btncancel);
         cancel.setOnClickListener(view -> {
-            //
+            String contenu_fchier = this.readContentOfFile(filename);
+            champ_text.setText(contenu_fchier);
         });
         //
         //bouton d'arret de l'activité
@@ -27,5 +49,55 @@ public class MainActivity2_question_4 extends AppCompatActivity {
         stop.setOnClickListener(view -> {
             this.finish();
         });
+        //
+    }
+    //methode qui cree un fichier s'il existe pas dans le contexte de l'application
+    private void createFileIfNoteExiste(String filename){
+        Context context = getApplicationContext(); // contexte de l'application
+        File file = new File(context.getFilesDir(), filename); //onbtention du fichier
+        if(!file.exists()){
+            try {
+                file.createNewFile(); //creation du fichier s'il n'existe pas
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    private void writeToFile(String contents,String filename){
+        Context context = getApplicationContext();
+        try (FileOutputStream fos = context.openFileOutput(filename, Context.MODE_PRIVATE)) {
+            fos.write(contents.getBytes(StandardCharsets.UTF_8));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finally {
+        }
+    }
+    //methode qui lit et renvoie le contenu du fichier
+    private String readContentOfFile(String filename){
+        Context context = getApplicationContext();
+        FileInputStream fis = null;
+        try {
+            fis = context.openFileInput(filename);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        InputStreamReader inputStreamReader = new InputStreamReader(fis, StandardCharsets.UTF_8);
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(inputStreamReader)) {
+            String line = reader.readLine();
+            while (line != null) {
+                stringBuilder.append(line).append('\n');
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            // Error occurred when opening raw file for reading.
+        } finally {
+            String contents = stringBuilder.toString();
+            return(contents);
+        }
+
     }
 }
